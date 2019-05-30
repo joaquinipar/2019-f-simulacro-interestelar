7b4f09357cd5429eaaf0dc18894bc1ed0a42a37a
joaquinipar <44877112+joaquinipar@users.noreply.github.com>
2019-05-30 19:56:09 -0300

    restore branch

--- interestelar.hs
+++ interestelar.hs
@@ -5,44 +5,86 @@ coordX (x,_,_) = x
 coordY (_,y,_) = y
 coordZ (_,_,z) = z
 
--- Punto 1
-
-data Planeta = Planeta{
+data Planeta = Planeta {
 
 posicion :: Posicion,
 dilatacion :: Float
-} 
+}
 
-data Astronauta = Astronauta{
+data Astronauta = Astronauta {
 
 edadTerrestre :: Float,
 planetaActual :: Planeta
 }
 
--- Punto 2: 
+x109Z = Planeta {
 
-x109z = Planeta {
-    posicion = (50,100,70),
-    dilatacion = 2.0
+posicion = (50,100,70),
+dilatacion = 2.0
 }
-laTierra = Planeta{
-    posicion = (0,0,0)
-    dilatacion = 0.0
+
+laTierra = Planeta {
+
+posicion = (0,0,0),
+dilatacion = 0.0
 }
 
 anastasia = Astronauta {
-    edadTerrestre = 22.0
-    planetaActual = laTierra
+
+edadTerrestre = 22.0,
+planetaActual = laTierra
 }
 
 bart = Astronauta{
-    edadTerrestre = 11.0,
-    planetaActual = x109z
+
+edadTerrestre = 11.0,
+planetaActual = x109Z
 }
 
 -- Punto 3
 
-distancia :: Planeta -> Planeta -> Float
-distancia = sqrt.(**2).
+distanciaX :: Planeta -> Planeta -> Float
+distanciaX (Planeta coordenadas1 _) (Planeta coordenadas2 _)  = (coordX coordenadas1 - coordX coordenadas2)**2
 
-resta planeta1 planeta2 = 
+distanciaY :: Planeta -> Planeta -> Float
+distanciaY (Planeta coordenadas1 _) (Planeta coordenadas2 _) = (coordY coordenadas1 - coordY coordenadas2)**2
+
+distanciaZ :: Planeta -> Planeta -> Float
+distanciaZ (Planeta coordenadas1 _) (Planeta coordenadas2 _) = (coordZ coordenadas1 - coordZ coordenadas2)**2
+
+distancia :: Planeta -> Planeta -> Float
+distancia planeta1 planeta2= sqrt(distanciaX planeta1 planeta2 + distanciaY planeta1 planeta2 + distanciaZ planeta1 planeta2 )
+
+-- Punto 4
+
+pasarAnios :: Astronauta -> Float -> Astronauta
+pasarAnios astronauta anios= astronauta{edadTerrestre = (edadTerrestre astronauta) + anios + (dilatacionDeAstronauta astronauta) }
+
+dilatacionDeAstronauta astronauta = dilatacion (planetaActual astronauta)
+
+-- Punto 5
+type Tiempo = Float
+
+type Velocidad = Float
+
+data Nave = Nave {
+
+planetaOrigen :: Planeta,
+planetaDestino :: Planeta,
+tanquesOxigeno :: Float,
+tipoX :: Char
+}
+
+laNaveVieja :: Nave -> Float
+laNaveVieja nave | (tanquesOxigeno nave) < 6 = 10
+                 | otherwise = 7
+
+laNaveFuturista _ = 0
+
+--laNaveX :: 
+laNaveX nave | tipoX == 'a' = 15
+             | tipoX == 'b' = 11
+             | otherwise = 11  -- agregar retraso
+
+tiempo :: (Nave -> Float)-> Nave -> Tiempo
+tiempo funcion nave velocidad= distancia (planetaOrigen nave) (planetaDestino nave)/
