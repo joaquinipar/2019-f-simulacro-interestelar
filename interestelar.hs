7a093986419b9f8bee2e1b0a052aa0ae8ddc6994
joaquinipar <44877112+joaquinipar@users.noreply.github.com>
2019-05-30 20:59:51 +0000

    v1

--- interestelar.hs
+++ interestelar.hs
@@ -5,3 +5,44 @@ coordX (x,_,_) = x
 coordY (_,y,_) = y
 coordZ (_,_,z) = z
 
+-- Punto 1
+
+data Planeta = Planeta{
+
+posicion :: Posicion,
+dilatacion :: Float
+} 
+
+data Astronauta = Astronauta{
+
+edadTerrestre :: Float,
+planetaActual :: Planeta
+}
+
+-- Punto 2: 
+
+x109z = Planeta {
+    posicion = (50,100,70),
+    dilatacion = 2.0
+}
+laTierra = Planeta{
+    posicion = (0,0,0)
+    dilatacion = 0.0
+}
+
+anastasia = Astronauta {
+    edadTerrestre = 22.0
+    planetaActual = laTierra
+}
+
+bart = Astronauta{
+    edadTerrestre = 11.0,
+    planetaActual = x109z
+}
+
+-- Punto 3
+
+distancia :: Planeta -> Planeta -> Float
+distancia = sqrt.(**2).
+
+resta planeta1 planeta2 = 
